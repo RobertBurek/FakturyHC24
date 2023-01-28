16d2903710cf63705810aa2f536e95fcbe7ed537
RobertB <robertburek@wp.pl>
2023-01-28 22:33:47 +0100

    Początek

---
+++ php/saveInvoice.php
@@ -0,0 +1,54 @@
+<?php
+
+$nameTable = $_POST['nameTable'];
+$word = $_POST['word'];
+$level = $_POST['level'];
+$attempt = $_POST['attempt'];
+$isCategory = ($_POST['isCategory'] == "true") ? '1' : '0';
+$isOnlyWord = ($_POST['isOnlyWord'] == "true") ? '1' : '0';
+$points = $_POST['points'];
+
+require_once "connect.php";
+
+$connection = @new mysqli($host, $db_user, $db_password, $db_name);
+if ($connection->connect_errno != 0) {
+    echo "Error: " . $connection->connect_errno . " Opis: " . $connection->connect_error;
+} else {
+
+    if (
+        $connection->query(sprintf(
+            "INSERT INTO `%s` (`Word`, `Level`, `Attempt`, `IsCategory`, `IsOnlyWord`, `Points`) VALUES ('%s', '%s', '%s', '%s', '%s', '%s');",
+            mysqli_real_escape_string($connection, $nameTable),
+            mysqli_real_escape_string($connection, $word),
+            mysqli_real_escape_string($connection, $level),
+            mysqli_real_escape_string($connection, $attempt),
+            mysqli_real_escape_string($connection, $isCategory),
+            mysqli_real_escape_string($connection, $isOnlyWord),
+            mysqli_real_escape_string($connection, $points)
+        ))
+    ) {
+        $resultLevel = $connection->query(sprintf(
+            "SELECT COUNT(*) FROM `%s` WHERE `%s`.`Level` = '%s';",
+            mysqli_real_escape_string($connection, $nameTable),
+            mysqli_real_escape_string($connection, $nameTable),
+            mysqli_real_escape_string($connection, $level)
+        ));
+
+        $countLevel = $resultLevel->fetch_assoc();
+        echo json_encode(array(
+            "result" => $countLevel['COUNT(*)'],
+            "level" => $level,
+            "error" => 'Zapisano słowo w bazie gracza !!!',
+            // "nameTable" => $nameTable,
+            // "word" => $word,
+            // "attempt" => $attempt,
+            // "isCategory" => $isCategory,
+            // "isOnlyWord" => $isOnlyWord,
+            "points" => $points
+        ));
+    } else {
+        echo json_encode(array("word" => $word, "error" => 'Słowo istniało już w bazie gracza !!!'));
+    }
+
+    $connection->close();
+}
---
+++ php/sendInvoice.php
@@ -0,0 +1,75 @@
+<?
+srand((double)microtime()*1000000);
+$znacznik = md5(uniqid(rand()));
+
+// dane o odbiorcy, nadawcy i załączniku
+$odbiorca = "homecare.24@wp.pl";
+$tytul = "Jestem zainteresowana/y ...";
+$nadawca_imie = "Szukam Pracy";
+$nadawca_email = "szukam@pracy.pl";
+// $plik = "logo.gif";
+// $plik = "add.gif";
+$plik = $_FILES['plik']['name'];
+// $plik = $_POST['plik'];
+// $typpliku = "image/gif";
+$typpliku = "application/pdf";
+// $nazwapliku = "mojelogo.pdf";
+$nazwapliku = $_FILES['plik']['name'];
+// $nazwapliku = $plik;
+$content = $_POST['contentMail'];
+
+
+
+
+// function function_alert($message) {
+      
+//     // Display the alert box 
+//     echo "<script>alert('$message');</script>";
+// }
+  
+  
+// Function call
+// function_alert($plik);
+// echo "<b>Nazwa pliku: </b>".$_FILES['plik']['name']."<br />";
+// echo "<b>Typ pliku: </b>".$_FILES['plik']['type']."<br />";
+// echo "<b>Rozmiar pliku: </b>".$_FILES['plik']['size']."<br />";
+// echo "<b>Nazwa pliku tymczasowego: </b>".$_FILES['plik']['tmp_name']."<br /><br />";
+
+
+
+// treść listu
+$tresclistu = '
+Witaj, kilka zdań o mnie: 
+" '.$content.' " ';
+
+// definicja nagłówków
+$naglowki  = "From: $nadawca_imie <$nadawca_email>\n";
+$naglowki .= "MIME-Version: 1.0\n";
+$naglowki .= "Content-Type: multipart/mixed;\n";
+$naglowki .= "\tboundary=\"___$znacznik==\"";
+
+// nagłówki listu
+$tresc="--___$znacznik==\n";
+$tresc .="Content-Type: text/plain; charset=utf-8\r\n";
+$tresc .="Content-Transfer-Encoding: 8bit\n";
+$tresc .="\n$tresclistu\n";
+
+// nagłówki i obsługa załącznika
+$tresc .="--___$znacznik==\n";
+// $tresc .="Content-Type: $typpliku\n";
+// $tresc .="Content-Type: $typpliku\n";
+$tresc .="Content-Disposition: attachment;\n";
+$tresc .=" filename=\"$nazwapliku\"\n";
+$tresc .="Content-Transfer-Encoding: base64\n\n";
+$f = fopen($_FILES['plik']['tmp_name'],"r");
+$dane = fread($f,filesize($_FILES['plik']['tmp_name']));
+fclose($f);
+$tresc .= chunk_split(base64_encode($dane));
+$tresc .="--___$znacznik==--\n";
+
+// wysłanie listu
+mail($odbiorca,$tytul,$tresc,$naglowki);
+
+header('Location: ' . $_SERVER['HTTP_REFERER'] . '#contact');
+
+?>
