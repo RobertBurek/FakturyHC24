f9757281fb5e69dace8298b1400d400cc4c11970
Robert Burek <robertburek@wp.pl>
2023-07-16 19:56:33 +0200

    deleteNews.php refaktor

--- php/deleteNews.php
+++ php/deleteNews.php
@@ -2,7 +2,6 @@
 
 $whoDel = $_POST['WhoDel'];
 $idNews = $_POST['IdNews'];
-// $estateNews =  $_POST['EstateNews'];
 
 require_once "connect.php";
 
@@ -12,7 +11,6 @@ if ($connection->connect_errno != 0) {
 } else {
     $whoDel = htmlentities($whoDel, ENT_QUOTES, "UTF-8");
     $delDate = date("Y-m-d H:i:s");
-    // $newscast = [];
 
     
         $connection->query(sprintf(
@@ -23,20 +21,6 @@ if ($connection->connect_errno != 0) {
             mysqli_real_escape_string($connection, $tb_newscast),
             mysqli_real_escape_string($connection, $idNews)
         ));
-        // sleep(2);
-        //  {
-        //     if ($resultNewscast = @$connection->query(sprintf(
-        //         // "SELECT * FROM `%s` WHERE `EstateNews`='%s' AND `IsDel`= 0 ORDER BY SaveDate DESC;",
-        //         // "SELECT * FROM `%s` WHERE `EstateNews`='%s' AND `IsDel`= 0 ORDER BY DateNews DESC;",
-        //         "SELECT * FROM `%s` WHERE `EstateNews`='%s' ORDER BY SaveDate DESC;",
-        //         mysqli_real_escape_string($connection, $tb_newscast),
-        //         mysqli_real_escape_string($connection, $estateNews)
-        //     ))) {
-        //         while ($rowInfo = $resultNewscast->fetch_row()) {
-        //             array_push($newscast, $rowInfo);
-        //         }
-                // echo json_encode($newscast);
-        //     };
-        // }
+
         $connection->close();
     }
